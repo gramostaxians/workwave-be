76fdd3b7e6303312da7ec5a0f6f3528f68bb7997
gramost(outlook) <gramos.thaqi@outlook.com>
2025-05-15 13:34:00 +0200

    Add spring project

---
+++ src/main/java/com/hr/workwave/WorkwaveApplication.java
@@ -0,0 +1,13 @@
+package com.hr.workwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkwaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hr/workwave/WorkwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.workwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
