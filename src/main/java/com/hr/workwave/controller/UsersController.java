a547c3ec7830f11b736bd31b33aa70a02e89fc63
ervin.segaqa <ervinsegaqa25@gmail.com>
2025-05-22 09:35:34 +0200

    Data

--- src/main/java/com/hr/workwave/controller/UsersController.java
+++ src/main/java/com/hr/workwave/controller/UsersController.java
@@ -1,14 +1,10 @@
 package com.hr.workwave.controller;
 
-
-import com.hr.workwave.model.LeaveRequest;
 import com.hr.workwave.model.Users;
-import com.hr.workwave.repo.UsersRepository;
 import com.hr.workwave.services.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
