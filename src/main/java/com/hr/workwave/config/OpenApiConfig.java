f66ccf615e1a858be5fe6d01d9894b8b9e646ff6
ervin.segaqa <ervinsegaqa25@gmail.com>
2025-05-30 11:27:30 +0200

    Add swagger config

--- src/main/java/com/hr/workwave/config/OpenApiConfig.java
+++ src/main/java/com/hr/workwave/config/OpenApiConfig.java
@@ -1,17 +1,29 @@
 package com.hr.workwave.config;
 
 
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("api")
+                .group("my-api")
                 .packagesToScan("com.hr.workwave.controller")
                 .build();
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("WorkWave API").version("1.0"))
+                .servers(List.of(new Server().url("https://s00-vecarbonapp/v2")));
     }
 }
