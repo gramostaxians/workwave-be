c47e2d3bda41bea3fe795623d9f8a54ed0438e09
ervin.segaqa <ervinsegaqa25@gmail.com>
2025-06-02 15:02:24 +0200

    latest changes

--- src/main/java/com/hr/workwave/Config/CorsConfig.java
+++
@@ -1,7 +0,0 @@
-package com.hr.workwave.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
