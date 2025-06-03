a4284c6f0989f6bfbb512349c1b29ae276a27834
ervin.segaqa <ervinsegaqa25@gmail.com>
2025-06-03 14:16:34 +0200

    update cors config

--- src/main/java/com/hr/workwave/WebConfig/WebConfig.java
+++ src/main/java/com/hr/workwave/WebConfig/WebConfig.java
@@ -14,10 +14,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://s00-vecarbonapp")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
