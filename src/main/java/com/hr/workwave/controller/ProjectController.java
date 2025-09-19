ba09a9d0064975b9dd392d806a91d02edb9de391
ervin.segaqa <ervinsegaqa25@gmail.com>
2025-09-19 13:24:44 +0200

    fix bug in ProjectController

--- src/main/java/com/hr/workwave/controller/ProjectController.java
+++ src/main/java/com/hr/workwave/controller/ProjectController.java
@@ -3,15 +3,15 @@ package com.hr.workwave.controller;
 import com.hr.workwave.model.Project;
 import com.hr.workwave.services.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Controller()
+@RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class ProjectController {
 
     private final ProjectService projectService;
