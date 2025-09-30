b036ddb52ddcf534dc628e633873e834a6fae8da
gramost(outlook) <gramos.thaqi@outlook.com>
2025-09-30 11:38:23 +0200

    Fix applying for HO #2

--- src/main/java/com/hr/workwave/repo/LeaveRequestRepository.java
+++ src/main/java/com/hr/workwave/repo/LeaveRequestRepository.java
@@ -49,7 +49,7 @@ public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long
                     @Param("endDate") LocalDate endDate);
 
 
-    @Query("SELECT COUNT(lr) FROM LeaveRequest lr WHERE lr.leave_type = 'HOME_OFFICE' AND lr.start_date <= :date AND lr.end_date >= :date AND lr.user.project = :projectId")
+    @Query("SELECT COUNT(lr) FROM LeaveRequest lr WHERE lr.leave_type = 'HOME_OFFICE' AND lr.start_date <= :date AND lr.end_date >= :date AND lr.user.project.id = :projectId")
     int countHomeOfficeRequestsOnDateAndProject(@Param("date") LocalDate date, @Param("projectId") BigInteger projectId);
 
     @Query("SELECT CASE WHEN COUNT(lr) > 0 THEN true ELSE false END FROM LeaveRequest lr " +
